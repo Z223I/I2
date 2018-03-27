bf6de5fe216fba7f1aa49eee3cfb2ce76777aac5
Z223I <BruceWilsonPH@yahoo.com.ph>
2018-03-27 10:20:12 -0500

    Added qs_partition and it's test methods.

--- Bubble/SASTest.cpp
+++ Bubble/SASTest.cpp
@@ -8,6 +8,31 @@
 #include <array>
 using namespace std;
 
+void print_array(int arr[], uint start, uint stop) {
+    for (uint i=start; i<=stop; i++) {
+        if (i < stop) {
+            cout << arr[i] << ", ";
+        } else {
+            cout << arr[i] << endl;
+        }
+    }
+
+}
+
+bool compare_subarrays(int arr1[], int arr2[], uint start, uint stop) {
+    bool b_equal = true;
+    uint i;
+
+    for (i=start; i<=stop; i++) {
+        if (arr1[i] != arr2[i]) {
+            b_equal = false;
+            break;
+        }
+    }
+
+    return b_equal;
+}
+
 // Registers the fixture into the 'registry'
 CPPUNIT_TEST_SUITE_REGISTRATION( MoneyTest );
 
@@ -199,63 +224,62 @@ MoneyTest::test_merge_sort_A()
 
 }
 
-
+//#define OIWEFJHOSIKDFJ
 void
 MoneyTest::test_qs_partition_A()
 {
 
-    Money::MyArray5 unpartitioned = {5, 1, 8, 2, 4};
-    Money::MyArray5 partitioned =   {1, 2, 4, 5, 8};
-    Money::MyArray5 result;
+    int unpartitioned[] = {5, 1, 8, 2, 4};
+    int partitioned[]   = {1, 2, 4, 5, 8};
 
     Money money;
     money = Money();
 
+    uint array_size = sizeof(unpartitioned) / sizeof(unpartitioned[0]);
+    
     uint start = 0;
-    uint stop = unpartitioned.size() - 1;
+    uint stop = array_size - 1;
 
-    money.merge_sort(unpartitioned, start, stop);
-    result = unpartitioned;
+    money.qs_partition(unpartitioned, start, stop);
 
-    if (result != partitioned) {
-        cout << "test_qs_partition result: ";
-        cout_5(result);
+    if ( !compare_subarrays(unpartitioned, partitioned, start, stop) ) {
+        cout << endl << "test_qs_partition result: ";
+        print_array(unpartitioned, start, stop);
         cout << endl;
         cout << "partitioned: ";
-        cout_5(partitioned);
+        print_array(partitioned, start, stop);
         cout << endl;
     }
-    CPPUNIT_ASSERT(result == partitioned);
+    CPPUNIT_ASSERT(compare_subarrays(unpartitioned, partitioned, start, stop));
 
 }
-
 
 void
 MoneyTest::test_qs_partition_B()
 {
 
-    array<int, 5> unpartitioned = {5, 1, 8, 2, 4};
-    Money::MyArray5 partitioned =   {1, 2, 4, 5, 8};
-    Money::MyArray5 result;
+    int unpartitioned[] = {10, 80, 30, 90, 40, 50, 70};
+    int partitioned[]   = {10, 30, 40, 50, 70, 90, 80};
 
     Money money;
     money = Money();
 
+    uint array_size = sizeof(unpartitioned) / sizeof(unpartitioned[0]);
+    
     uint start = 0;
-    uint stop = unpartitioned.size() - 1;
+    uint stop = array_size - 1;
 
-    money.merge_sort(unpartitioned, start, stop);
-    result = unpartitioned;
+    money.qs_partition(unpartitioned, start, stop);
 
-    if (result != partitioned) {
-        cout << "test_qs_partition result: ";
-        cout_5(result);
+    if ( !compare_subarrays(unpartitioned, partitioned, start, stop) ) {
+        cout << endl << "test_qs_partition result: ";
+        print_array(unpartitioned, start, stop);
         cout << endl;
         cout << "partitioned: ";
-        cout_5(partitioned);
+        print_array(partitioned, start, stop);
         cout << endl;
     }
-    CPPUNIT_ASSERT(result == partitioned);
+    CPPUNIT_ASSERT(compare_subarrays(unpartitioned, partitioned, start, stop));
 
 }
 
--- Bubble/comp.sh
+++ Bubble/comp.sh
@@ -1,2 +1,2 @@
+echo "g++ -g -std=c++11 -lcppunit SASApp.cpp SASTest.cpp -o SAS.x"
 g++ -g -std=c++11 -lcppunit SASApp.cpp SASTest.cpp -o SAS.x
-
